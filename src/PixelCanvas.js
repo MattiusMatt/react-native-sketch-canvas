2adaa851a3f7df116a64cb7a70faaf9e03b41358
Mattius <matt.hartley@ascentms.co.uk>
2020-07-12 00:37:30 +0100

    Fixed Android iOS PixArt coordinate issues

--- src/PixelCanvas.js
+++ src/PixelCanvas.js
@@ -1,5 +1,5 @@
-import React, { ReactNode, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import React, { ReactNode, useState, useEffect } from "react";
+import { View, PanResponder, StyleSheet } from "react-native";
 
 interface Props {
     pixelSize: number;
@@ -14,68 +14,76 @@ interface Props {
 export default (props: React.PropsWithChildren<Props>) => {
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
-    const columns: number = Math.floor(width / props.pixelSize);
-    const rows: number = Math.floor(height / props.pixelSize);
-    const offset: number = (width % props.pixelSize) / 2;
+    const [rows, setRows] = useState(0);
+    const [columns, setColumns] = useState(0);
+    const [offset, setOffset] = useState(0);
+    const [pixBoardTop, setPixBoardTop] = useState(0);
+    const [pixBoardLeft, setPixBoardLeft] = useState(0);
 
     let cells: Array<ReactNode> = [];
     let paths: Array<string> = [];
 
-    const onTouch = (event, top, left) => {
+    const onTouch = (event, gestureState) => {
         paths = [];
-        props.cellClicked(left, top, props.pixelSize, props.pixelSize);
-    };
 
-    const onMove = (event, top, left, row, col) => {
-        const xMovedOverBy: number = Math.floor(
-            event.nativeEvent.locationX / props.pixelSize
-        );
-        const yMovedOverBy: number = Math.floor(
-            event.nativeEvent.locationY / props.pixelSize
-        );
-        const path: string = `${xMovedOverBy},${yMovedOverBy}`;
+        const x: float = gestureState.x0 - pixBoardLeft;
+        const y: float = gestureState.y0 - pixBoardTop;
+
+        const col: float = Math.floor(x / props.pixelSize);
+        const row: float = Math.floor(y / props.pixelSize);
+
+        const path: string = `${col},${row}`;
 
         // Check not already coloured in
         if (paths.indexOf(path) > -1) {
             return;
         }
 
-        // Check not out of bounds
-        if (col + xMovedOverBy < 0) {
-            return;
-        }
-        if (col + xMovedOverBy >= columns) {
-            return;
-        }
-        if (row + yMovedOverBy >= rows) {
-            return;
-        }
+        paths.push(path);
 
         props.cellClicked(
-            left + xMovedOverBy * props.pixelSize,
-            top + yMovedOverBy * props.pixelSize,
+            col * props.pixelSize,
+            row * props.pixelSize,
             props.pixelSize,
             props.pixelSize
         );
 
+        console.log(`${col} ${row}`);
+    };
+
+    const onMove = (event, gestureState) => {
+        const x: float = gestureState.moveX - pixBoardLeft;
+        const y: float = gestureState.moveY - pixBoardTop;
+
+        const col: float = Math.floor(x / props.pixelSize);
+        const row: float = Math.floor(y / props.pixelSize);
+
+        const path: string = `${col},${row}`;
+
+        // Check not already coloured in
+        if (paths.indexOf(path) > -1) {
+            return;
+        }
+
         paths.push(path);
+
+        props.cellClicked(
+            col * props.pixelSize,
+            row * props.pixelSize,
+            props.pixelSize,
+            props.pixelSize
+        );
+
+        console.log(`${col} ${row}`);
     };
 
     for (let row = 0; row < rows; row++) {
         for (let col = 0; col < columns; col++) {
             const top: number = row * props.pixelSize;
-            const left: number = col * props.pixelSize + offset;
+            const left: number = col * props.pixelSize;
 
             cells.push(
                 <View
-                    onStartShouldSetResponder={() => true}
-                    onStartShouldSetResponderCapture={() => true}
-                    onMoveShouldSetResponder={() => true}
-                    onMoveShouldSetResponderCapture={() => true}
-                    onResponderStart={(event) => onTouch(event, top, left)}
-                    onResponderMove={(event) =>
-                        onMove(event, top, left, row, col)
-                    }
                     key={`${row}-${col}`}
                     style={[
                         Style.pixel,
@@ -91,19 +99,63 @@ export default (props: React.PropsWithChildren<Props>) => {
         }
     }
 
+    useEffect(() => {
+        this.pixelBoard.measure((fx, fy, width, height, px, py) => {
+            setPixBoardLeft(px);
+            setPixBoardTop(py);
+        });
+    });
+
+    this.panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (event, gestureState) => true,
+        onStartShouldSetPanResponderCapture: (event, gestureState) => true,
+        onMoveShouldSetPanResponder: (event, gestureState) => true,
+        onMoveShouldSetPanResponderCapture: (event, gestureState) => true,
+        onShouldBlockNativeResponder: (event, gestureState) => true,
+        onPanResponderGrant: (event, gestureState) => {
+            onTouch(event, gestureState);
+        },
+        onPanResponderMove: (event, gestureState) => {
+            onMove(event, gestureState);
+        },
+        onPanResponderRelease: (event, gestureState) => {
+            console.log("up");
+        },
+    });
+
     return (
         <View
-            onStartShouldSetResponder={() => true}
             style={Style.container}
             onLayout={(event) => {
                 const { width, height } = event.nativeEvent.layout;
+                const rows: number = Math.floor(height / props.pixelSize);
+                const columns: number = Math.floor(width / props.pixelSize);
+                const offset: number = (width % props.pixelSize) / 2;
 
                 setWidth(width);
                 setHeight(height);
+                setColumns(columns);
+                setRows(rows);
+                setOffset(offset);
             }}
         >
-            {props.children}
-            {cells}
+            <View
+                ref={(view) => {
+                    this.pixelBoard = view;
+                }}
+                style={[
+                    Style.pixelBoard,
+                    {
+                        left: offset,
+                        width: columns * props.pixelSize,
+                        height: rows * props.pixelSize,
+                    },
+                ]}
+                {...this.panResponder.panHandlers}
+            >
+                {props.children}
+                {cells}
+            </View>
         </View>
     );
 };
@@ -111,6 +163,11 @@ export default (props: React.PropsWithChildren<Props>) => {
 const Style = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    pixelBoard: {
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderColor: "red",
     },
     pixel: {
         position: "absolute",
